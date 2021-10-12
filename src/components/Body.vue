93d458bc0768a89499dce33abaed504bf620d76e
Zeyu-Li <zeyu.li23@gmail.com>
2021-10-11 23:45:07 -0600

    init project + get ins

---
+++ src/components/Body.vue
@@ -0,0 +1,63 @@
+<template>
+  <div style="height: 50%; width: 100%; display: flex; justify-content: center">
+    <textarea
+      style="width: 100%; height: 50%; text-align: left"
+      rows="14"
+      placeholder="Input LaTeX here"
+      class="form-control"
+    ></textarea>
+  </div>
+
+  <div style="height: 50%" class="outputSection">
+    <hr />
+    <div style="display: flex; justify-content: space-between">
+      <p>Output Text:</p>
+      <button
+        type="button"
+        title="Copy output to clipboard"
+        class="btn btn-primary copyButton"
+      >
+        Copy Output to Clipboard 📋
+      </button>
+    </div>
+    <code id="output"> </code>
+  </div>
+</template>
+
+<script>
+// const area = document.querySelector("textarea")
+// const output = document.getElementById("output")
+// const button = document.querySelector(".copyButton")
+// const nonsense = "#*%@^"
+// let outputString = ""
+
+// area.addEventListener('input', () => {
+//     outputString = ""
+//     const newInput = area.value.replaceAll("\\$", nonsense)
+
+//     let start = true
+//     for (let i = 0; newInput && i < newInput.length; i++) {
+//         if (newInput[i] === "$") {
+//             outputString += start ? "\\(" : "\\)"
+//             start = !start
+//         } else {
+//             outputString += newInput[i]
+//         }
+//     }
+//     // convert nonsense back to regular dollar sign
+//     output.innerText = outputString.replaceAll(nonsense, "$")
+// }, false)
+// button.addEventListener('click', () => {
+//     navigator.clipboard.writeText(outputString)
+// }, false)
+
+export default {
+  name: "Body",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
